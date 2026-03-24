e4fc9d2b95b369b9672190f7d2356b33ce186b53
Jaideep Kumar Singh <jaideepkumarsingh14@gmail.com>
2026-03-24 19:49:05 +0530

    Completed the Generic Chatbot with UI using Streamlit

---
+++ Projects/generic_chatbot_with_ui/frontend.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from backend import chatbot
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input = st.chat_input("Type here...")
+if user_input:
+    with st.chat_message("user"):
+        st.session_state["message_history"].append(
+            {"role": "user", "content": user_input}
+        )
+        st.text(user_input)
+
+    config = {"configurable": {"thread_id": 1}}
+    ai_message = chatbot.invoke(
+        {"messages": [HumanMessage(content=user_input)]}, config=config
+    )
+    ai_message = ai_message["messages"][-1].content
+    with st.chat_message("ai"):
+        st.session_state["message_history"].append(
+            {"role": "ai", "content": ai_message}
+        )
+        st.text(ai_message)
---
+++ Projects/generic_chatbot_with_ui/frontend_streaming.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from backend import chatbot
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input = st.chat_input("Type here...")
+if user_input:
+    with st.chat_message("user"):
+        st.session_state["message_history"].append(
+            {"role": "user", "content": user_input}
+        )
+        st.text(user_input)
+
+    config = {"configurable": {"thread_id": 1}}
+    with st.chat_message("ai"):
+        ai_message = st.write_stream(
+            message_chunk.content
+            for message_chunk, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=config,
+                stream_mode="messages",
+            )
+        )
+        st.session_state["message_history"].append(
+            {"role": "ai", "content": ai_message}
+        )
